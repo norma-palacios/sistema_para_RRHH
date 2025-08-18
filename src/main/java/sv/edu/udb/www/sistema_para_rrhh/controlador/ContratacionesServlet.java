cf5217b88a9ba4e2c3b75689054200fd02f21a19
norma-palacios <pr242879@alumno.udb.edu.sv>
2025-08-18 10:48:30 -0600

    mostrar listado de contrataciones

--- src/main/java/sv/edu/udb/www/sistema_para_rrhh/controlador/ContratacionesServlet.java
+++ src/main/java/sv/edu/udb/www/sistema_para_rrhh/controlador/ContratacionesServlet.java
@@ -1,20 +1,21 @@
 package sv.edu.udb.www.sistema_para_rrhh.controlador;
 
 import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
 import sv.edu.udb.www.sistema_para_rrhh.dao.ContratacionesDao;
-import sv.edu.udb.www.sistema_para_rrhh.modelo.Contratacion;
+import sv.edu.udb.www.sistema_para_rrhh.modelo.ListaContratacion;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
+@WebServlet("/ContratacionesServlet")
 public class ContratacionesServlet extends HttpServlet {
 ContratacionesDao dao = new ContratacionesDao();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      List<Contratacion> lista = dao.listar();
+      List<ListaContratacion> lista = dao.listar();
       request.setAttribute("listaContrataciones", lista);
       RequestDispatcher rd = request.getRequestDispatcher("vista/Home.jsp");
       rd.forward(request,response);
--- src/main/java/sv/edu/udb/www/sistema_para_rrhh/dao/ContratacionesDao.java
+++ src/main/java/sv/edu/udb/www/sistema_para_rrhh/dao/ContratacionesDao.java
@@ -1,67 +1,55 @@
 package sv.edu.udb.www.sistema_para_rrhh.dao;
 
-import sv.edu.udb.www.sistema_para_rrhh.modelo.Contratacion;
+import sv.edu.udb.www.sistema_para_rrhh.conexion.ConexionBD;
+import sv.edu.udb.www.sistema_para_rrhh.modelo.ListaContratacion;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class ContratacionesDao
 {
-    public List<Contratacion> listar(){
-        List<Contratacion> contratacionesMock = new ArrayList<>();
-        contratacionesMock.add(
-                new Contratacion(
-                        0,
-                        "RRhhh",
-                        "norma",
-                        "abogado",
-                        "plaza fija",
-                        new Date(),
-                        1000.00,
-                        "activo"
-                )
-        );
+    public List<ListaContratacion> listar(){
+        List<ListaContratacion> lista = new ArrayList<>();
 
-        contratacionesMock.add(
-                new Contratacion(
-                        0,
-                        "RRhhh",
-                        "diego",
-                        "abogado",
-                        "plaza fija",
-                        new Date(),
-                        1000.00,
-                        "activo"
-                )
-        );
+        String sql = "SELECT c.idContratacion, c.fechaContratacion, c.salario, c.estado,"+
+                       "d.nombreDepartamento,"+
+                       "e.nombrePersona,"+
+                       "ca.cargo,"+
+                       "t.tipoContratacion "+
+                "FROM contrataciones c "+
+                "INNER JOIN departamento d ON c.idDepartamento = d.idDepartamento "+
+                "INNER JOIN empleados e ON c.idEmpleado = e.idEmpleado "+
+                "INNER JOIN cargos ca ON c.idCargo = ca.idCargo "+
+                "INNER JOIN tipoContratacion t ON c.idTipoContratacion = t.idTipoContratacion "+
+                "ORDER BY c.idContratacion DESC";
 
-        contratacionesMock.add(
-                new Contratacion(
-                        0,
-                        "RRhhh",
-                        "maria",
-                        "abogado",
-                        "plaza fija",
-                        new Date(),
-                        1000.00,
-                        "activo"
-                )
-        );
+        try {
+            Connection con = ConexionBD.getConnection();
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
 
-        contratacionesMock.add(
-                new Contratacion(
-                        0,
-                        "RRhhh",
-                        "benjamin",
-                        "abogado",
-                        "plaza fija",
-                        new Date(),
-                        1000.00,
-                        "activo"
-                )
-        );
+            while (rs.next()) {
+                ListaContratacion listaContratacion = new ListaContratacion();
+                listaContratacion.setIdContratacion(rs.getInt("idContratacion"));
+                listaContratacion.setFechaContratacion(rs.getDate("fechaContratacion").toLocalDate());
+                listaContratacion.setSalario(rs.getDouble("salario"));
+                listaContratacion.setEstado(rs.getBoolean("estado"));
+                listaContratacion.setDepartamento(rs.getString("nombreDepartamento"));
+                listaContratacion.setEmpleado(rs.getString("nombrePersona"));
+                listaContratacion.setCargo(rs.getString("cargo"));
+                listaContratacion.setTipoContratacion(rs.getString("tipoContratacion"));
 
-        return contratacionesMock;
+                lista.add(listaContratacion);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return lista;
     }
 }
